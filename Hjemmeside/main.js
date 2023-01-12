b893b4239d6a70d471f09a7c52f8f17399ca52c7
HiImKobeAnd <hiimkobeand@gmail.com>
2023-01-12 12:12:51 +0100

    Added some pocketbase functionality

--- Hjemmeside/main.js
+++ Hjemmeside/main.js
@@ -7,8 +7,15 @@ document.querySelector('#app').innerHTML = `
   <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
   </div>
 `
-const pb = new Pocketbase('http://192.168.1.23:8090');
+const pb = new Pocketbase('http://192.168.1.23:80');
 
+let response = await pb.collection("temp").getFullList(100, {sort: "created"})
+
+pb.collection("temp").subscribe("*", function (e) {
+  console.log(e.record)
+})
+
+//console.log(response)
 
 let xValues = [50,60,70,80,90,100,110,120,130,140,150];
 let yValues = [7,8,8,9,9,9,10,11,14,14,15];
